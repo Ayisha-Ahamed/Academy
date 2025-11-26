1dd4fbd00e53ac90b155190508dacbeb31de3fe2
Ayisha <Ayisha.Ahamed@trumpf.com>
2025-11-26 17:13:50 +0530

    Review fix 3

--- A04/Program.cs
+++ A04/Program.cs
@@ -8,8 +8,8 @@
 // ------------------------------------------------------------------------------------------------
 using static System.Console;
 
-var list = File.ReadLines ("words.txt").SelectMany(a=>a);
 Dictionary<char, int> table = [];
-foreach (var ch in list) table[ch] = 1 + (table.TryGetValue (ch, out int num) ? num : 0);
+foreach (var ch in File.ReadLines ("words.txt").SelectMany (a => a))
+   table[ch] = 1 + (table.TryGetValue (ch, out int num) ? num : 0);
 WriteLine ("The seven most frequently used letters are:");
 foreach (var w in table.OrderByDescending (a => a.Value).Take (7)) WriteLine ($"{w.Key}: {w.Value}");
