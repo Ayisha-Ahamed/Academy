28f518500bb64e851a9b82f83ea1fa188be87c98
Ayisha <Ayisha.Ahamed@trumpf.com>
2025-11-28 19:32:49 +0530

    Review fix 1: Updated FindNumber() method

--- A02.1/Program.cs
+++ A02.1/Program.cs
@@ -4,48 +4,29 @@
 // ------------------------------------------------------------------
 // Program.cs
 // A02.1: Guessing game (MSB to LSB)
-// Program prints the number the user thinks of (0 - 255) using binary search from MSB to LSB.
+// Program guesses a random number (0-255) using binary search from MSB to LSB.
 // ------------------------------------------------------------------------------------------------
-using System.Text;
 using static System.Console;
 
 do {
    Clear ();
-   WriteLine ("Think of a number between 0 - 255");
-   WriteLine ($"\nYour number is {FindNumber ()}");
-   Write ("Press 'Y' to play again");
+   Write ($"Think of a number between 0 - 255");
+   Write ($"\nYour number is {FindNumber ()}\nPress 'Y' to play again");
 } while (ReadKey (true).Key == ConsoleKey.Y);
 
-// Returns the number the user thinks of.
-// Gets user response at each step.
+// Returns the random number calculated from user response.
 static int FindNumber () {
-   int max = 256, min = 0;
-   StringBuilder num = new ();
+   int max = 256, min = 0, num = 0;
    for (int i = 0; i < 8; i++) {
       int mid = (max + min) / 2;
-      Write ($"Is the number less than {mid}? (y/n) ");
-      switch (GetKey ()) {
-         case ConsoleKey.Y: max = mid; num.Append ('0'); break;
-         case ConsoleKey.N: min = mid; num.Append ('1'); break;
-      }
-   }
-   return BinaryToInt (num.ToString ());
-}
-
-// Returns integer value of binary string.
-static int BinaryToInt (string binStr) {
-   int num = 0, len = binStr.Length, pow = len - 1;
-   for (int i = 0; i < len; i++) {
-      num += binStr[i] == '1' ? (int)Math.Pow (2, pow) : 0;
-      pow--;
+      Write ($"\nIs the number less than {mid}? (y/n) ");
+      ConsoleKey key = ReadKey (true).Key; // Gets user response.
+      while (!(key is ConsoleKey.Y or ConsoleKey.N)) key = ReadKey (true).Key;
+      Write (key == ConsoleKey.Y ? "Yes" : "No");
+      if (key == ConsoleKey.N) {
+         num += (int)Math.Pow (2, 7 - i); // Converts binary to decimal.
+         min = mid;
+      } else max = mid;
    }
    return num;
-}
-
-// Gets user response(Y/N) and returns the key pressed.
-static ConsoleKey GetKey () {
-   ConsoleKey key = ReadKey (true).Key;
-   while (!(key is ConsoleKey.Y or ConsoleKey.N)) key = ReadKey (true).Key;
-   Write ($"{(key == ConsoleKey.Y ? "Yes" : "No")}\n");
-   return key;
 }
