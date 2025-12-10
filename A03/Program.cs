f57a7f965ed9146a72667f4777968452140d2c7d
Ayisha <Ayisha.Ahamed@trumpf.com>
2025-12-10 18:35:11 +0530

    Review fix 6: Updated print method, fixed formatting

--- A03/Program.cs
+++ A03/Program.cs
@@ -8,20 +8,30 @@
 // ------------------------------------------------------------------------------------------------
 using static System.Console;
 
+const int NLETTERS = 7; // Number of letters in spelling bee puzzle
+
 Write ("Enter seven distinct letters: ");
 string input = (ReadLine () ?? "").Trim ().ToUpper ();
-if (input.All (char.IsLetter) && input.Distinct ().Count () == 7) {
+if (input.All (char.IsLetter) && input.Distinct ().Count () == NLETTERS) {
    var words = File.ReadLines ("words.txt")
-   .Where (w => IsValid (w, input)).Select (GetScore) // Filter valid words into a tuple.
-   .OrderByDescending (w => w.Score).ToList ();
-   words.ForEach (PrintScore);
+   .Where (w => IsValid (w, input))
+   .Select (GetScore) // Filter valid words into a tuple
+   .OrderByDescending (w => w.Score)
+   .ToList ();
+   foreach (var word in words) {
+      if (word.IsPangram) {
+         ForegroundColor = ConsoleColor.Green;
+         PrintScore (word);
+         ResetColor ();
+      } else PrintScore (word);
+   }
    WriteLine ($"----\n{words.Sum (w => w.Score)} total");
 } else WriteLine ("Invalid input. Please enter seven distinct alphabetic characters.");
 
 // Returns a tuple consisting of input, whether the input is a pangram and its score
 static (string Word, bool IsPangram, int Score) GetScore (string word) {
-   bool isPangram = word.Distinct ().Count () == 7;
-   int len = word.Length, score = len == 4 ? 1 : len + (isPangram ? 7 : 0);
+   bool isPangram = word.Distinct ().Count () == NLETTERS;
+   int len = word.Length, score = len == 4 ? 1 : len + (isPangram ? NLETTERS : 0);
    return (word, isPangram, score);
 }
 
@@ -29,10 +39,4 @@ static (string Word, bool IsPangram, int Score) GetScore (string word) {
 static bool IsValid (string w, string input) => w.Length > 3 && w.Contains (input[0]) && !w.Except (input).Any ();
 
 // Prints the score to console
-static void PrintScore ((string Word, bool IsPangram, int Score) tp) {
-   if (tp.IsPangram) {
-      ForegroundColor = ConsoleColor.Green;
-      WriteLine ($"{tp.Score, 3}. {tp.Word}");
-      ResetColor ();
-   } else WriteLine ($"{tp.Score, 3}. {tp.Word}");
-}
+static void PrintScore ((string Word, bool IsPangram, int Score) tp) => WriteLine ($"{tp.Score,3}. {tp.Word}");
