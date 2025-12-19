7dd7a725425d82fcc492df210caaf8f4b14b90c0
Ayisha <Ayisha.Ahamed@trumpf.com>
2025-12-19 13:27:22 +0530

    Review fix 10

--- A03/Program.cs
+++ A03/Program.cs
@@ -12,20 +12,24 @@ const int NLETTERS = 7; // Number of letters in spelling bee puzzle
 
 Write ("Enter seven distinct letters: ");
 string input = (ReadLine () ?? "").Trim ().ToUpper ();
+int total = 0; // Stores the total score of the words in the solution.
 if (input.All (char.IsLetter) && IsPangram (input)) {
    List<(ConsoleColor Color, string Word, int Score)> words = [.. File.ReadLines ("words.txt")
-                                                              .Where (w => IsValid (w, input)).Select (GetScore)];
-   foreach (var (Color, Word, Score) in words.OrderBy (w => w.Color).ThenByDescending (w => w.Score)) {
+                                                               .Where (w => IsValid (w, input)).Select (GetScore)
+                                                               .OrderByDescending (w => w.Score)];
+   foreach (var (Color, Word, Score) in words) {
       ForegroundColor = Color;
+      // Right align score with a width of 3.
       WriteLine ($"{Score, 3}. {Word}");
+      total += Score;
    }
    ResetColor ();
-   WriteLine ($"----\n{words.Sum (w => w.Score)} total");
+   WriteLine ($"----\n{total} total");
 } else WriteLine ("Invalid input. Please enter seven distinct alphabetic characters.");
 
 // Returns a tuple consisting of console foreground color, word and score
 static (ConsoleColor Color, string Word, int Score) GetScore (string word) {
-   (ConsoleColor color, int score) = IsPangram (word) ? (ConsoleColor.Green, NLETTERS) : (ConsoleColor.White, 0);
+   (ConsoleColor color, int score) = IsPangram (word) ? (ConsoleColor.Green, NLETTERS) : (ConsoleColor.Gray, 0);
    int len = word.Length;
    return (color, word, score + (len == 4 ? 1 : len));
 }
