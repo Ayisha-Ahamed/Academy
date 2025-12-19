c16b1d1ea5820d6a1c3674649f260e49eb012b3f
Ayisha <Ayisha.Ahamed@trumpf.com>
2025-12-19 17:48:28 +0530

    Added ShiftOneByN method

--- A02.2/Program.cs
+++ A02.2/Program.cs
@@ -8,30 +8,32 @@
 // ------------------------------------------------------------------------------------------------
 using static System.Console;
 
-const int LSB = 1; // LSB of the number.
-
 do {
    Clear ();
-   Write ("Think of a number between 0 and 255\nIs your number odd? (y/n) ");
+   Write ("Think of a number between 0 and 255\n");
    Write ($"\nYour number is {FindNumber ()}\nPress 'Y' to play again");
 } while (ReadKey (true).Key == ConsoleKey.Y);
 
 // Returns the user guessed number
 static int FindNumber () {
-   // Initialize LSB of the number
-   int num = IsYPressed () ? LSB : 0, rem;
+   Write ("Is your number odd? (y/n) ");
+   // If the number is odd, set LSB to 1
+   int num = IsYPressed () ? 1 : 0, rem;
    // Prompts the user input to find (8 - n)th MSB where n ranges from 1 to 7
    for (int n = 1; n <= 7; n++) {
-      rem = (LSB << n) + num;
-      Write ($"Does your number leave a remainder of {rem} when divided by {LSB << (n + 1)}? (y/n) ");
+      rem = ShiftOneByN (n) + num;
+      Write ($"Does your number leave a remainder of {rem} when divided by {ShiftOneByN (n + 1)}? (y/n) ");
       if (IsYPressed ()) num = rem;
    }
    return num;
 }
 
-// Returns if the user pressed 'Y'.
+// Returns if the user pressed 'Y'
 static bool IsYPressed () {
    bool isYPressed = ReadKey (true).Key == ConsoleKey.Y;
    WriteLine (isYPressed ? "Yes" : "No");
    return isYPressed;
 }
+
+// Left shifts bit 1 by n positions
+static int ShiftOneByN (int n) => 1 << n;
