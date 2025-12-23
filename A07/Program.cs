c5992cd62d760ce83d16a24de274c5a756d9cd7f
Ayisha <Ayisha.Ahamed@trumpf.com>
2025-12-23 15:16:27 +0530

    Implemented Parse class

--- A07/Program.cs
+++ A07/Program.cs
@@ -6,40 +6,108 @@
 // A07: Program to parse string into double.
 // ------------------------------------------------------------------------------------------------
 
-using System.Text.RegularExpressions;
 using static System.Console;
 
 namespace A07;
 
 class Program {
-   static Regex regex = new (@"^(\+|-)?([0-9]*)(\.[0-9]+)?((e|E)(\+|-)?[0-9]+)?$");
 
    static void Main () {
       do {
          Clear ();
          Write ("Enter number: ");
          var input = ReadLine () ?? "";
-         if (regex.IsMatch (input)) WriteLine (Parse (input.ToLower ()));
-         else WriteLine ("Entered value is not a double");
+         if (TryParse (input, out double num)) WriteLine (num);
+         else WriteLine ("Not a double");
          Write ("Press 'Y' to continue");
       } while (ReadKey (true).Key == ConsoleKey.Y);
    }
 
-   // Returns double converted from input string.
-   static double Parse (string str) {
-      double num = 0;
-      // Length of number without exponent.
-      int nLen = str.Contains ('e') ? str.IndexOf ('e') : str.Length;
-      string dec = str[0] == '+' || str[0] == '-' ? str[1..nLen] : str[..nLen];
-      // Calculate index position of decimal point.
-      int point = dec.Contains ('.') ? dec.IndexOf ('.') : dec.Length, e = 0;
-      // Conversion of significant digits to double.
-      foreach (var ch in dec.Where (char.IsDigit)) num += (ch - '0') * Math.Pow (10, --point);
-      // Conversion of exponent component to double.
-      if (str.Length > nLen + 1) {
-         foreach (var c in str[nLen..].Where (char.IsDigit)) e = (e * 10) + (c - '0');
-         num = num * Math.Pow (10, str[nLen + 1] == '-' ? -e : +e);
+   // Returns true if the input string is a valid double
+   static bool TryParse (string input, out double num) {
+      num = Double.NaN;
+      try {
+         num = new Parse (input).Value;
+         return true;
+      } catch {
+         return false;
       }
-      return str[0] == '-' ? -num : num;
+   }
+
+   // Extracting double from string takes place by dividing double into parts such as whole, fraction and exponent
+   // Each part is made up of integers with whole and exponent parts having  possible signed values
+   class Parse {
+      #region Enums -------------------------------------------------------------------------------
+      // Tracks the current part of the double (whole, fraction, exponent) processed by the methods
+      enum State {
+         Whole,
+         Fraction,
+         Exponent
+      }
+      #endregion
+
+      #region Properties---------------------------------------------------------------------------
+      // Returns double value converted from input string
+      public double Value => mValue;
+      #endregion
+
+      #region Constructor -------------------------------------------------------------------------
+      public Parse (string text) {
+         mText = text;
+         mN = 0;
+         mValue = GetDouble ();
+      }
+      #endregion
+
+      #region Private Methods ---------------------------------------------------------------------
+      // Returns the number converted from the string
+      int GetLiteral () {
+         int num = 0;
+         while (mN < mText.Length && char.IsDigit (mText[mN]))
+            num = (num * 10) + (mText[mN++] - '0');
+         return num;
+      }
+
+      // Returns signed number converted from string
+      int GetSignedLiteral () {
+         int sign = 1;
+         if (mText[mN] is '+' or '-')
+            if (mText[mN++] is '-') sign = -1;
+         return GetLiteral () * sign;
+      }
+
+      // Returns double converted from input string
+      double GetDouble () {
+         double parsed = GetSignedLiteral ();
+         State prevState = State.Whole;
+         while (mN < mText.Length) {
+            char c = mText[mN++];
+            switch (c) {
+               case '.': {
+                     if (prevState is State.Whole) {
+                        int currentIndex = mN;
+                        double fraction = GetLiteral () * Math.Pow (0.1, mN - currentIndex);
+                        parsed += parsed < 0 ? -fraction : fraction;
+                        prevState = State.Fraction; break;
+                     } else throw new Exception ();
+                  }
+               case 'e': {
+                     if (prevState is State.Whole or State.Fraction) {
+                        parsed *= Math.Pow (10, GetSignedLiteral ());
+                        prevState = State.Exponent; break;
+                     } else throw new Exception ();
+                  }
+               default: throw new Exception ();
+            }
+         }
+         return parsed;
+      }
+      #endregion
+
+      #region Private Data ---------------------------------------------------------------------
+      readonly string mText; // Stores input string
+      int mN;          // Points to the next index position to be read
+      double mValue;   // Stores double converted from string
+      #endregion ----------------------------------------------------------------------------------
    }
 }
