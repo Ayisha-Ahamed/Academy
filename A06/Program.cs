cdf28889a3df9dee2a814c899445ff409248cdb7
Ayisha <Ayisha.Ahamed@trumpf.com>
2025-12-24 17:45:05 +0530

    Review fix 1: Updated AddIfValid method

--- A06/Program.cs
+++ A06/Program.cs
@@ -14,57 +14,50 @@ using static Math;
 class Program {
    static void Main () {
       OutputEncoding = Encoding.UTF8;
-      var solver = new SolveEightQueens ();
-      solver.PrintBoard ();
+      WriteLine ("The twelve unique solutions of 8 queens are: ");
+      _ = new SolveEightQueens ();
    }
 }
 
 class SolveEightQueens {
    #region Constants-------------------------------------------------------------------------------
-   // Dimension of chess board (n x n).
+   // Dimension of chess board (n x n)
    const int DIM = 8;
    #endregion--------------------------------------------------------------------------------------
 
    #region Public Methods--------------------------------------------------------------------------
    // Prints chess board on the console
-   public void PrintBoard () {
-      for (int n = 0; n < mSolved.Count; n++) {
-         Clear ();
-         List<int> board = mSolved[n]; // nth solution
-         WriteLine ($"Solution no: {n + 1}");
-         WriteLine ("\n┏━━━┳━━━┳━━━┳━━━┳━━━┳━━━┳━━━┳━━━┓");
-         for (int row = 0; row < DIM; row++) {
-            Write ("┃ ");
-            int posOfQueen = board[row]; // Stores the position of queen in current row
-            for (int col = 0; col < DIM; col++) Write (col == posOfQueen ? "♛ ┃ " : "  ┃ ");
-            if (row != 7) WriteLine ("\n┣━━━╋━━━╋━━━╋━━━╋━━━╋━━━╋━━━╋━━━┫");
-         }
-         WriteLine ("\n┗━━━┻━━━┻━━━┻━━━┻━━━┻━━━┻━━━┻━━━┛");
-         Write ("Press 'N' to move next");
-         while (ReadKey (true).Key != ConsoleKey.N) ;
+   void PrintBoard (int[] board) {
+      WriteLine ($"\nSolution no: {mSolved.Count}");
+      WriteLine ("┏━━━┳━━━┳━━━┳━━━┳━━━┳━━━┳━━━┳━━━┓");
+      for (int row = 0; row < DIM; row++) {
+         Write ("┃ ");
+         int posOfQueen = board[row]; // Stores the position of queen in current row
+         for (int col = 0; col < DIM; col++) Write (col == posOfQueen ? "♛ ┃ " : "  ┃ ");
+         if (row != 7) WriteLine ("\n┣━━━╋━━━╋━━━╋━━━╋━━━╋━━━╋━━━╋━━━┫");
       }
+      WriteLine ("\n┗━━━┻━━━┻━━━┻━━━┻━━━┻━━━┻━━━┻━━━┛");
    }
    #endregion--------------------------------------------------------------------------------------
 
    #region Constructors----------------------------------------------------------------------------
    public SolveEightQueens () {
-      if (mSolved.Count == 0) {
-         // Place queen in first row ith column and calculate
-         for (int i = 0; i < DIM; i++) SolveNthQueen (1, [i]);
-      }
+      // Place queen in first row nth column and solve
+      for (int n = 0; n < DIM; n++) SolveNthQueen (1, [n]);
    }
 
    #endregion -------------------------------------------------------------------------------------
 
-   #region Private Methods-------------------------------------------------------------------------
+   #region Helper Methods-------------------------------------------------------------------------
    // Returns all possible column positions for nth row
-   static List<int> GetValidColPos (int row, List<int> sol) {
+   List<int> GetValidColPos (int row, List<int> sol) {
       List<int> validColPos = [];
+      bool isValid;
+      // Eliminate previously occupied column positions
       foreach (var col in Enumerable.Range (0, DIM).Except (sol)) {
-         if (sol.Contains (col)) continue;
-         bool isValid = true;
+         isValid = true;
          for (int i = 0; i < sol.Count; i++) {
-            // Verify column position is not affected by queens placed in diagonal position.
+            // Check if current column is safe from other queens diagonally
             if (Abs (row - i) == Abs (col - sol[i])) {
                isValid = false; break;
             }
@@ -74,50 +67,55 @@ class SolveEightQueens {
       return validColPos;
    }
 
-   // Adds list n to solution if n is canonical.
-   static void AddIfValid (List<int> n) {
-      int[] nRotated = [.. n];
-      for (int i = 0; i < 4; i++) {
-         nRotated = Rotate (nRotated);
-         if (IsDuplicate (HMirror (nRotated)) || IsDuplicate (VMirror (nRotated)) || IsDuplicate (nRotated)) return;
+   // Returns true if the solution is canonical
+   bool IsValid (int[] sol) => !(IsDuplicate (HMirror (sol)) || IsDuplicate (VMirror (sol)) || IsDuplicate (sol));
+
+   // Adds canonical solution to mSolved
+   void AddIfValid (List<int> n) {
+      int[] sol = [.. n];
+      if (!IsValid (sol)) return;
+      for (int i = 0; i < 3; i++) {
+         sol = Rotate (sol); // Rotate solution by 90°
+         if (!IsValid (sol)) return;
       }
-      mSolved.Add (n);
+      mSolved.Add (sol);
+      PrintBoard (sol);
    }
 
-   // Returns if the solution is duplicate.
-   static bool IsDuplicate (int[] sol) => mSolved.Any (arr => arr.SequenceEqual (sol));
+   // Returns true if the solution is duplicate
+   bool IsDuplicate (int[] sol) => mSolved.Any (arr => arr.SequenceEqual (sol));
 
-   // Returns the vertical mirror of array n.
-   static int[] VMirror (int[] n) => [.. n.Reverse ()];
+   // Returns the vertical mirror of the solution
+   int[] VMirror (int[] sol) => [.. sol.Reverse ()];
 
-   // Returns the horizontal mirror of array n.
-   static int[] HMirror (int[] n) {
+   // Returns the horizontal mirror of the solution
+   int[] HMirror (int[] sol) {
       var arr = new int[DIM];
-      for (int i = 0; i < DIM; i++) arr[i] = DIM - n[i] - 1;
+      for (int i = 0; i < DIM; i++) arr[i] = DIM - sol[i] - 1;
       return arr;
    }
 
-   // Returns n rotated by 90 degrees in clockwise direction.
-   static int[] Rotate (int[] n) {
+   // Returns solution rotated by 90° in clockwise direction
+   int[] Rotate (int[] sol) {
       int[] arr = new int[DIM];
-      for (int i = 0; i < DIM; i++) arr[n[i]] = DIM - i - 1;
+      for (int i = 0; i < DIM; i++) arr[sol[i]] = DIM - i - 1;
       return arr;
    }
 
-   // Search for position(s) to place nth queen in nth row.
-   static void SolveNthQueen (int n, List<int> currentSol) {
+   // Adds valid column position for nth queen with respect to current solution
+   void SolveNthQueen (int n, List<int> currentSol) {
       if (n == DIM) {
          AddIfValid (currentSol);
          return;
       }
-      // If multiple solutions are found, the current solution is copied to the next iteration.
+      // If multiple solutions are found, the current solution is copied to the next iteration
       foreach (var pos in GetValidColPos (n, currentSol))
          SolveNthQueen (n + 1, [.. currentSol, pos]);
    }
    #endregion--------------------------------------------------------------------------------------
 
    #region Private Data----------------------------------------------------------------------------
-   // Stores the solved results.
-   static List<List<int>> mSolved = [];
+   // Stores the solved results
+   static List<int[]> mSolved = [];
    #endregion
 }
