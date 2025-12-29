6dc57f5f91069d96155ac073650a5ea98f0e5e57
Ayisha-Ahamed <Ayisha.Ahamed@trumpf.com>
2025-12-29 09:30:11 +0530

    A02.2: Guessing game (LSB to MSB) (#5)
    * A02.2: Initial commit
    
    * Updated comments
    
    * Formatting update
    
    * Review fix 1
    
    * Updated comments and formatting
    
    * Review fix 2: Applied review changes
    
    * Review fix 3: Updated program description
    
    * Naming and comments updated
    
    * Added ShiftOneByN method
    
    * Updated methods
    
    * Fixed comments
    
    * Review fix 4: Updated methods

---
+++ A02.2/Program.cs
@@ -0,0 +1,31 @@
+﻿// ------------------------------------------------------------------------------------------------
+// Training ~ A training program for new joinees at Metamation, Batch- July 2025.
+// Copyright (c) Metamation India.
+// ------------------------------------------------------------------
+// Program.cs
+// A02.2: Guessing game (LSB to MSB).
+// Program to find the number between 0 and 255 based on the user response.
+// ------------------------------------------------------------------------------------------------
+using static System.Console;
+
+do {
+   Clear ();
+   Write ("Think of a number between 0 and 255\n");
+   Write ($"\nYour number is {FindNumber ()}\nPress 'Y' to play again");
+} while (ReadKey (true).Key == ConsoleKey.Y);
+
+// Returns the user guessed number
+static int FindNumber () {
+   int num = 0, rem, pow;
+   // Prompts the user input to find nth bit where n ranges from 0 to 7
+   for (int n = 0; n <= 7; n++) {
+      pow = 1 << n; // Stores the value of 2 ^ n
+      rem = pow + num;
+      Write ($"Does your number leave a remainder of {rem} when divided by {2 * pow}? (y/n) ");
+      if (ReadKey (true).Key == ConsoleKey.Y) {
+         WriteLine ("Yes");
+         num = rem;
+      } else WriteLine ("No");
+   }
+   return num;
+}
