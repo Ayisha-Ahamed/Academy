1e0f0a38f5613431a6262d5090a2a3520ff950db
Ayisha <Ayisha.Ahamed@trumpf.com>
2026-01-23 11:21:15 +0530

    Review fix

--- A07/Program.cs
+++ A07/Program.cs
@@ -12,21 +12,26 @@ namespace A07;
 #region class Program -----------------------------------------------------------------------------
 class Program {
    static void Main () {
-      string[] testcase = ["23.","123.45","+123.45","++123.45" ,"123.45e.45", "-123.45e5", "123.45.45",
-         "123.45e-4", "123.45e--4", "123e45e2", "123abc", "e34", ".32", "0.32", "abc23de", "1e", "++","-e"];
-      foreach (var test in testcase) {
-         bool isDouble = double.TryParse (test, out double parsed);
-         Write ($"{test, -10}  |  ");
-         PrintResult (TryParse (test, out double num) && num == parsed || !isDouble ? "Pass" : "Fail");
-      }
+      TestParse ();
       Write ("Press 'Y' to continue");
       while (ReadKey (true).Key == ConsoleKey.Y) {
          Clear ();
          Write ("Enter number: ");
-         var input = (ReadLine () ?? "").Trim ().ToLower ();
-         if (TryParse (input, out double num)) WriteLine (num);
-         else WriteLine ("Not a double");
+         var input = (ReadLine () ?? "").Trim ();
+         WriteLine (TryParse (input, out double num) ? num : "Not a double");
          Write ("Press 'Y' to continue");
+      }
+      // Function to test class Parse
+      static void TestParse () {
+         string[] testcase = ["23.","123.45","+123.45","++123.45" ,"123.45e.45", "-123.45e5", "123.45.45",
+         "123.45e-4", "123.45e--4", "123e45e2", "123abc", "e34",".e34","e", ".32", "0.32", "abc23de", "1e", "++","-e","",
+         "1-23","-12-3.45e3","123.-4e4","123.45e3.6","abc","123.g","123e","123+","123-","+-98","12E3","-123.-98",".e-","-e+",
+         "123e4e5","0","2e+","4e-","+", "NaN"];
+         foreach (var test in testcase) {
+            bool isDouble = double.TryParse (test, out double num), isParsed = TryParse (test, out double parsed);
+            Write ($"{test,-10}  |  {num,-10}  |  {parsed,-10}  |  ");
+            PrintResult (isDouble == isParsed && num == parsed ? "Pass" : "Fail");
+         }
       }
       // Displays test results to the console
       static void PrintResult (string result) {
@@ -66,7 +71,7 @@ class Parse {
 
    #region Implementation -------------------------------------------
    // Returns the number converted from the string
-   int GetLiteral () {
+   int GetNum () {
       int num = 0;
       while (mIdx < mInput.Length && char.IsDigit (mInput[mIdx]))
          num = (num * 10) + (mInput[mIdx++] - '0');
@@ -74,42 +79,39 @@ class Parse {
    }
 
    // Returns signed number converted from string
-   int GetSignedLiteral () {
-      if (mIdx >= mInput.Length) throw new Exception ();
-      bool isNegative = false;
-      if (mInput[mIdx] is '+' or '-') {
-         isNegative = mInput[mIdx] is '-';
-         mIdx++;
-      }
-      return GetLiteral () * (isNegative ? -1 : 1);
+   int GetSignedNum () {
+      int sign = mInput[mIdx] is '+' or '-' && mInput[mIdx++] is '-' ? -1 : 1, start = mIdx, num = GetNum();
+      if (mIdx - start == 0) throw new Exception ("Sign operator should be followed by an integer");
+      return sign * num;
    }
 
    // Returns double converted from input string
    double GetDouble () {
-      double num = GetSignedLiteral (); // Get the whole part of the decimal number
+      double num = 0; // Get the whole part of the decimal number
       // Tracks the current state of the double processed
-      EParsed prevState = EParsed.Whole;
+      EParsed state = EParsed.None;
       while (mIdx < mInput.Length) {
-         switch (mInput[mIdx++]) {
-            case '.': {
-                  if (prevState is EParsed.Whole) {
-                     int start = mIdx, len;
-                     double fraction = GetLiteral () * Math.Pow (0.1, len = mIdx - start);
-                     num += num < 0 ? -fraction : fraction;
-                     // Round number up to converted decimal places
-                     num = Math.Round (num, len);
-                     prevState = EParsed.Fraction; break;
-                  } else throw new Exception ();
-               }
-            case 'e': {
-                  if (prevState is EParsed.Whole or EParsed.Fraction) {
-                     num *= Math.Pow (10, GetSignedLiteral ());
-                     prevState = EParsed.Exponent; break;
-                  } else throw new Exception ();
-               }
-            default: throw new Exception ();
+         switch (mInput[mIdx], state) {
+            case ('+' or '-' or (>= '0' and <= '9'), EParsed.None):
+               num = GetSignedNum ();
+               state = EParsed.Whole; break;
+            case ('.', EParsed.None or EParsed.Whole):
+               int start = ++mIdx, len;
+               double f = GetNum () * Math.Pow (0.1, len = mIdx - start);
+               if (len == 0 && start != mInput.Length)
+                  throw new Exception ("A decimal point should follow atleast one number");
+               num += num < 0 ? -f : f;
+               // Round number up to converted decimal places
+               num = Math.Round (num, len);
+               state = EParsed.Fraction; break;
+            case ('e' or 'E', EParsed.Whole or EParsed.Fraction):
+               mIdx++;
+               num *= Math.Pow (10, GetSignedNum ());
+               state = EParsed.Exponent; break;
+            default: throw new Exception ("Not a double");
          }
       }
+      if (state == EParsed.None) throw new Exception ("Input is empty");
       return num;
    }
    #endregion
@@ -125,11 +127,12 @@ class Parse {
 #region enum EParsed ------------------------------------------------------------------------------
 /// <summary>Defines the parsing states used when converting a numeric string into a double</summary>
 public enum EParsed {
-   /// <summary>Indicates that digits before decimal point (whole part) have been parsed</summary>
+   None,
+   // Indicates that digits before decimal point (whole part) have been parsed</summary>
    Whole,
-   /// <summary>Indicates that digits after decimal point (fractional part) have been parsed</summary>
+   // Indicates that digits after decimal point (fractional part) have been parsed</summary>
    Fraction,
-   /// <summary>Indicates that digits following 'e' or 'E' (exponent part) have been parsed</summary>
+   // Indicates that digits following 'e' or 'E' (exponent part) have been parsed</summary>
    Exponent
 }
 #endregion
