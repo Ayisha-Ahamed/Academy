c6b053eb0c478c2fe40e2f8929dbe835847bbba9
Ayisha <Ayisha.Ahamed@trumpf.com>
2026-02-11 13:38:18 +0530

    A14: Initial commit

---
+++ A14/Program.cs
@@ -0,0 +1,22 @@
+﻿// ------------------------------------------------------------------------------------------------
+// Training ~ A training program for new joinees at Metamation, Batch- July 2025.
+// Copyright (c) Metamation India.
+// ------------------------------------------------------------------
+// Program.cs
+// A14: Anagrams
+// Program displays anagrams in words.txt
+// ------------------------------------------------------------------------------------------------
+
+namespace A14;
+
+using static System.Console;
+
+class Program {
+   static void Main () {
+      var query = File.ReadLines ("words.txt")
+                      .GroupBy (a => new string ([.. a.Order ()]))
+                      .Where (a => a.Count () > 1)
+                      .OrderByDescending (a => a.Count ());
+      foreach (var anagram in query) WriteLine ($"{string.Join (' ', anagram)}");
+   }
+}
